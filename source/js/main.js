d24abb6895f4cba375347effd278d4eddb2d03ea
34comeOn <80600393+34comeOn@users.noreply.github.com>
2022-04-27 15:24:52 +0300

    offers 33%

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,36 @@
 import Glide from '@glidejs/glide';
 
-// new Glide('.glide').mount();
+const mainWrapperElement = document.querySelector('.wrapper');
+const videoCoverElement = document.querySelector('.about__video-cover');
+const videoContainerElement = document.querySelector('.about__video-container');
+const tabsContainerElement = document.querySelector('.subscription__button-list');
+const tabsListElement = document.querySelector('.subscription__tab-list');
+const videoElement = document.querySelector('.about__video');
+
+videoCoverElement.classList.remove('visually-hidden');
+
+function playVideo(media) {
+  media.play();
+}
+
+mainWrapperElement.classList.remove('wrapper--nojs');
+
+videoContainerElement.addEventListener('click', () => {
+  if (!videoCoverElement.classList.contains('visually-hidden')) {
+    videoCoverElement.classList.add('visually-hidden');
+    playVideo(videoElement);
+  }
+});
+
+tabsContainerElement.addEventListener('click', (evt) => {
+  if (!evt.target.classList.contains('subscription__button-tab--active') && evt.target.classList.contains('subscription__button-tab')) {
+    tabsContainerElement.querySelector('.subscription__button-tab--active').classList.remove('subscription__button-tab--active');
+    evt.target.classList.add('subscription__button-tab--active');
+    tabsListElement.querySelector('.subscription__card-list--active').classList.remove('subscription__card-list--active');
+    document.getElementById(`${evt.target.id}-subscription`).classList.add('subscription__card-list--active');
+  }
+});
+
 
 const glide = new Glide('.glide', {
   type: 'carousel',
@@ -19,34 +49,3 @@ const glide = new Glide('.glide', {
 });
 
 glide.mount();
-
-const mainWrapperElement = document.querySelector('.wrapper');
-const videoCoverElement = document.querySelector('.about__video-cover');
-const videoContainerElement = document.querySelector('.about__video-container');
-const tabsContainerElement = document.querySelector('.subscription__button-list');
-const tabsListElement = document.querySelector('.subscription__tab-list');
-// const videoElement = document.querySelector('.about__video');
-
-// videoCoverElement.classList.remove('visually-hidden');
-
-// function playVideo(media) {
-//   media.click();
-// }
-
-mainWrapperElement.classList.remove('wrapper--nojs');
-
-videoContainerElement.addEventListener('click', () => {
-  if (!videoCoverElement.classList.contains('visually-hidden')) {
-    videoCoverElement.classList.add('visually-hidden');
-    // playVideo(videoElement);
-  }
-});
-
-tabsContainerElement.addEventListener('click', (evt) => {
-  if (!evt.target.classList.contains('subscription__button-tab--active') && evt.target.classList.contains('subscription__button-tab')) {
-    tabsContainerElement.querySelector('.subscription__button-tab--active').classList.remove('subscription__button-tab--active');
-    evt.target.classList.add('subscription__button-tab--active');
-    tabsListElement.querySelector('.subscription__card-list--active').classList.remove('subscription__card-list--active');
-    document.getElementById(`${evt.target.id}-subscription`).classList.add('subscription__card-list--active');
-  }
-});
